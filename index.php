73ebbcd700892ce13ace3a6668fd5299ea853017
Kostya <Kpstya@mail.ru>
2020-08-05 22:56:29 +0300

    new color

--- index.php
+++ index.php
@@ -13,9 +13,10 @@
 </head>
 <body>
     <?php
+
     	echo <<<php
     	<h1>{$h1}</h1>
-    	<h1 style="color: red">{$text}</h1>
+    	<h1 style="color: blue">{$text}</h1>
     	<p style="font-size: 22px">Разбор примеров из задания #3<br>объяснения закомментированы</p>
 php;
 
@@ -23,6 +24,7 @@ php;
 		var_dump((int)'012345'); //с точки зрения целых чисел число с 0 в начале и без него, это - одно и то же число
 		var_dump((float)123.0 === (int)123.0); //false, так как разные типы данных
 		var_dump((int)0 === (int)'hello, world'); //true, так как в строке 'hello, world' не нашлось числа, поэтому автоматически 0
+
 	?>
 	<p style="font-size: 22px">Разбор последнего задания</p>
 	<?php
